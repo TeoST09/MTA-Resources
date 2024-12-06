8be8b5777cafaba3205ceadf17c969c55310b4a2
Mateo Lopez <mateo09lopez18@gmail.com>
2024-12-05 19:09:58 -0500

    2

--- Taxi/client.lua
+++ Taxi/client.lua
@@ -1,3 +1,9 @@
+local markerTrabajo = createMarker(1758.50537, -1894.01099, 12.55594, "cylinder", 3.0, 0, 0, 255, 255, false)
+createBlipAttachedTo(markerTrabajo, 42, 2, 255, 255, 255, 255)
+local trabajando = false
+local index = 1
+local markerInicio = nil
+local cor = nil
 
 function addNotification(texto, tipo)
     exports.Notificaciones:addNotification(texto, tipo, true)
@@ -5,45 +11,25 @@ end
 addEvent("addNotification", true)
 addEventHandler("addNotification", root, addNotification)
 
-
-
-local marker = createMarker(1758.50537, -1894.01099, 13.55594-1, "cylinder", 3.0, 0, 0, 255, 255, false)
-
-local isPlayerInMarker = false
-local trabajo = false
-
 function MarkerHit(hitPlayer)
     if isElement(hitPlayer) and getElementType(hitPlayer) == "player" and hitPlayer == localPlayer then
-        if not trabajo then
-        isPlayerInMarker = true
-        addNotification("Para Empezar a Trabajar pon /trabajar", "info", true) 
-        outputChatBox("/trabajar", 255, 0, 0)
+        if not trabajando and isElementWithinMarker(hitPlayer, markerTrabajo) then
+            addNotification("Para Empezar a Trabajar pon /trabajar", "info", true) 
+            outputChatBox("/trabajar", 255, 0, 0)
         end
     end
 end
-addEventHandler("onClientMarkerHit", marker, MarkerHit)
+addEventHandler("onClientMarkerHit", markerTrabajo, MarkerHit)
  
-
-function LeaveHit()
-    isPlayerInMarker = false
-end
-addEventHandler ( "onClientMarkerLeave", marker, LeaveHit )
-
-
-
-function Command(player)
-    if isPlayerInMarker then
-        if not trabajo then
-        addNotification("Bienvenido al Trabajo. Coje un Taxi para Empezar", "success", true) 
-        addNotification("Recuerda el /dejar para dejar Trabajo", "info", true) 
-        outputChatBox("/dejar para dejar Trabajo", 0,255,0)
-        trabajo = true
-        hola = createMarker(1787.86255, -1886.16650, 13.39508-1, "cylinder", 3.0, 0, 0, 255, 255, false)
-        addEventHandler("onClientMarkerHit", hola, Vehiclehit)
-        if isElement(marker) then
-            destroyElement(marker)
-            marker = nil 
-        end
+function starJob(player)
+    if isElementWithinMarker(localPlayer, markerTrabajo) then
+        if not trabajando then
+            trabajando = true
+            markerInicio = createMarker(1787.86255, -1886.16650, 13.39508-1, "cylinder", 3.0, 0, 0, 255, 255, false)
+            addNotification("Bienvenido al Trabajo. Coje un Taxi para Empezar", "success", true) 
+            addNotification("Recuerda el /dejar para dejar Trabajo", "info", true) 
+            addEventHandler("onClientMarkerHit", markerInicio, Vehiclehit)
+            isElementToDestroy(markerTrabajo)
         else
             addNotification("Ya Tienes este Trabajo", "warn", true) 
         end
@@ -51,14 +37,14 @@ function Command(player)
         addNotification("No Estas en el Marcador", "warn", true) 
     end
 end
-addCommandHandler("trabajar", Command)
-
+addCommandHandler("trabajar", starJob)
 
 function Vehiclehit()
-    if trabajo then
-        triggerServerEvent("onVehicle", root)
+    if trabajando then
+        triggerServerEvent("onVehicle", resourceRoot)
         addNotification("Sigue la Ruta para Empezar a Trabajar", "success", true)
-        destroyElement(hola)
+        isElementToDestroy(markerInicio)
+        createRecursiveMarker(index)
     end
 end
 
@@ -73,63 +59,51 @@ local marcadores = {
     {1817.3918457031, -1870.0146484375, 12.4140625}
 }
 
-local habi = createMarker(1787.86255, -1886.16650, 13.39508-1, "cylinder", 3.0, 0, 0, 0, 0, false)
-local index = 1
-local cor = nil
-local bli = 1
-
 function createRecursiveMarker(index)
-
     if index > #marcadores then
-        return -- Si el índice supera el número de marcadores, se detiene la recursión
+        return
     end
-
     
     local coor = marcadores[index]
     cor = createMarker(coor[1], coor[2], coor[3], "checkpoint", 4.0, 0, 0, 255, 255)
     blip = createBlip (coor[1], coor[2], coor[3],  0,  2,  255,  0,  0,  255,  0,  6383.0  )
     addEventHandler("onClientMarkerHit", cor, function(player)
         if player == getLocalPlayer() then
-            destroyElement(cor)
-            destroyElement(blip)
-            triggerServerEvent("onmoney", root)
+            isElementToDestroy(cor)
+            isElementToDestroy(blip)
+            triggerServerEvent("onmoney", resourceRoot)
             addNotification("Sigue Así", "success", true)
-            createRecursiveMarker(index + 1) -- Llamada recursiva para crear el siguiente marcador
+            createRecursiveMarker(index + 1)
             if index >= 8 then
                 addNotification("Vuelve al Marcador", "info", true) 
-                if not isElement(hola) then
-                    hola = createMarker(1787.86255, -1886.16650, 13.39508-1, "cylinder", 3.0, 0, 0, 255, 150, false)
-                    addEventHandler("onClientMarkerHit", hola, Vehiclehit)
+                if not isElement(markerInicio) then
+                    markerInicio = createMarker(1787.86255, -1886.16650, 13.39508-1, "cylinder", 3.0, 0, 0, 255, 150, false)
+                    addEventHandler("onClientMarkerHit", markerInicio, Vehiclehit)
+                    triggerServerEvent("onDestroyVehicle", resourceRoot)
                 end
             end
         end
     end)
 end
 
--- Evento inicial para comenzar el proceso
-
-function Activar()
-    if trabajo then
-        createRecursiveMarker(index)
+function Leave(player)
+    if trabajando then
+        trabajando = false
+        isElementToDestroy(markerInicio)
+        isElementToDestroy(cor)
+        isElementToDestroy(blip)
+        addNotification("Haz Dejado el Trabajo", "success", true) 
+        markerTrabajo = createMarker(1758.50537, -1894.01099, 13.55594-1, "cylinder", 3.0, 0, 0, 255, 255, false)
+        addEventHandler("onClientMarkerHit", markerTrabajo, MarkerHit)
+        triggerServerEvent("onDestroyVehicle", resourceRoot)
     else
-        return
+        addNotification("No Tienes este Trabajo", "error", true) 
     end
 end
-addEventHandler("onClientMarkerHit", habi, Activar)
-
-
-function Leave(player)
-    if trabajo then
-        trabajo = false
-            destroyElement(hola)
-            destroyElement(cor)
-            destroyElement(blip)
-            addNotification("Haz Dejado el Trabajo", "success", true) 
-                marker = createMarker(1758.50537, -1894.01099, 13.55594-1, "cylinder", 3.0, 0, 0, 255, 255, false)
-                addEventHandler("onClientMarkerHit", marker, MarkerHit)
-                triggerServerEvent("onDestroyVehicle", root)
-        else
-            addNotification("No Tienes este Trabajo", "error", true) 
-        end
-end
 addCommandHandler("dejar", Leave)
+
+function isElementToDestroy(element)
+    if isElement(element) then
+        destroyElement(element)
+    end
+end
--- Taxi/server.lua
+++ Taxi/server.lua
@@ -1,7 +1,7 @@
-
 local playerVehicles = {}
+
 addEvent("onVehicle", true)
-addEventHandler("onVehicle", resourceRoot, function(player)
+addEventHandler("onVehicle", resourceRoot, function()
     local player = client
     
     if playerVehicles[player] then
@@ -9,35 +9,26 @@ addEventHandler("onVehicle", resourceRoot, function(player)
             destroyElement(playerVehicles[player])
         end
     end
-       
-        local taxi = createVehicle(420, 1835.21216, -1872.24207, 13.38975 + 1)
-        if taxi then
-            warpPedIntoVehicle(player, taxi)
-            playerVehicles[player] = taxi
-            
-            setTimer(function()
-                if isElement(taxi) then
-                    destroyElement(taxi)
-                    triggerClientEvent(player, "addNotification", player, "Paso el Tiempo de Prestado de Taxi", "warn", true)
-                    playerVehicles[player] = nil
-                end
-            end, 10000000, 1)
 
-    else
-        triggerClientEvent(player, "addNotification", player, "No se Pudo Crear el Taxi", "warn", true)
-    end
-
+    playerVehicles[player] = createVehicle(420, 1835.21216, -1872.24207, 14.38975)
+    warpPedIntoVehicle(player, playerVehicles[player])
+    
+    setTimer(function()
+        if isElement(playerVehicles[player]) then
+            destroyElement(playerVehicles[player])
+            triggerClientEvent(player, "addNotification", player, "Paso el Tiempo de Prestado de Taxi", "warn", true)
+            playerVehicles[player] = nil
+        end
+    end, 10000000, 1)
 end)
 
-
-function money ( player )
-    local player = client
-    givePlayerMoney ( player, 2500 )
+function money ()
+    givePlayerMoney ( client, 2500 )
 end
 addEvent("onmoney", true)
 addEventHandler("onmoney", resourceRoot, money)
 
-function onDestroyVehicle ( player )
+function onDestroyVehicle()
     local player = client
     if playerVehicles[player] then
         if isElement(playerVehicles[player]) then
@@ -47,6 +38,3 @@ function onDestroyVehicle ( player )
 end
 addEvent("onDestroyVehicle", true)
 addEventHandler("onDestroyVehicle", resourceRoot, onDestroyVehicle)
-
-
-
